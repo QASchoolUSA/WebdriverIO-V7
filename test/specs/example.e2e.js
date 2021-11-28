4c5a277cc71a0bd65f2db6108638f3a55a0b08c2
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2021-11-27 21:37:55 -0500

    Added first test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,1 +1,20 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url('https://www.google.com/');
 
+        await $('//input[@name="q"]').setValue('SDET');
+        await browser.keys('Enter');
+
+        await expect(browser).toHaveTitle('SDET - Google Search');
+
+        const results = await $$('.g .tF2Cxc .yuRUbf a');
+        const hrefs = [];
+        
+        for(let i = 0; i < results.length; i++) {
+            hrefs.push(results[i].getAttribute('href'));
+            browser.pause(3000);
+            browser.url(hrefs[i]);
+        }
+        
+    });
+});
